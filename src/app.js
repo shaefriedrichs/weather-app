c63e6ecc2ec08d93196bb6517f73d8e3e0a8b684
Shae <shaefriedrichs@gmail.com>
2023-04-19 09:46:02 +1200

    Reinjected HTML into JS

--- src/app.js
+++ src/app.js
@@ -21,6 +21,35 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thur", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+                            <div class="weather-forecast-date">
+                                ${day}
+                            </div>
+                            <div>🌧️</div>
+                            <div class="weather-forecast-temperatures">
+                                <span class="weather-forecast-temperature-max">
+                                    18
+                                </span>
+                                <span class="weather-forecast-temperature-min">
+                                    12
+                                </span>
+                            </div>
+                        </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -89,3 +118,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Auckland");
+displayForecast();
